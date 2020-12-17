2c97dbf0e6dc49c5e256ad13325423513eccac06
MsPockets <mcnutterika@gmail.com>
2020-12-17 17:55:00 -0500

    Add auto scroll feature

--- src/components/Chats.svelte
+++ src/components/Chats.svelte
@@ -1,13 +1,23 @@
 <script>
   import {Collection} from 'sveltefire'
   import AppendMessage from './AppendMessage.svelte'
+  import {tick} from 'svelte'
+  import * as animateScroll from 'svelte-scrollto'
   export let user
+
+  async function newMessageScroll() {
+    console.log('data received')
+    await tick()
+    animateScroll.scrollToBottom()
+  
+  }
+  let chatBox 
 </script>
-<Collection path={'/chat'} let:ref={chatsRef} let:data={messages}>
-  <div class="box chat-box">
+<Collection path={'/chat'} let:ref={chatsRef} let:data={messages}  on:data={newMessageScroll}>
+<div class="box chat-box" id="chat-box" bind:this={chatBox}>
     {#each messages.sort((a,b) => a.date - b.date) as {message}}
       <div class="notification">
-        {message}
+        {user.name}:{message}
       </div>
     {/each}
   </div>
